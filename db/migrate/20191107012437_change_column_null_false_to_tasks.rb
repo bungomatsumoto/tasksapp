6b8321e1dccd5b32fec8af91ba5a0d4d94a9e56a
bungo matsumoto <matsumotobg@gmail.com>
2019-11-07 10:28:12 +0900

    DBの制約をマイグレーションファイルで作成

---
+++ db/migrate/20191107012437_change_column_null_false_to_tasks.rb
@@ -0,0 +1,6 @@
+class ChangeColumnNullFalseToTasks < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :tasks, :title, null: false
+    change_column_null :tasks, :explanation, null: false
+  end
+end

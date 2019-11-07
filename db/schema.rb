f107581422a62c97203d2a7d53d0712dfeefcc28
bungo matsumoto <matsumotobg@gmail.com>
2019-11-07 10:19:47 +0900

    schemaをコミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_015931) do
+ActiveRecord::Schema.define(version: 2019_11_06_073443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_015931) do
     t.text "explanation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "deadline", default: "2019-11-06", null: false
   end
 
 end
